b63bbbc830f4e36960600bd5dccedd669d73c3e3
SachinP997 <130444012+SachinP997@users.noreply.github.com>
2023-05-19 17:27:52 +0530

    Add files via upload

---
+++ employees.js
@@ -0,0 +1,18 @@
+const employees=[
+    {
+        id:1,
+        emp_name:'John Varghese',
+        emp_designation:'IT manager',
+        emp_address:'Pune'
+
+    },
+    {
+        id:2,
+        emp_name:'Mohan Kumar',
+        emp_designation:'IT manager',
+        emp_address:'Germany'
+
+    }
+]
+
+module.exports=employees
